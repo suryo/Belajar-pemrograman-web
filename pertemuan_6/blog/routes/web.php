bf0aea4b930b67e2fea56d4d1a9641fd7ec7445c
fahrizal-aulia <fahrizalaulia@gmail.com>
2023-10-30 19:55:00 +0700

    pertemuan_6 laravel

--- pertemuan_6/blog/routes/web.php
+++ pertemuan_6/blog/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/asik', function () {
+    return view('asik');
+});
+Route::get('/kucing', function () {
+    return view('asik');
+});
